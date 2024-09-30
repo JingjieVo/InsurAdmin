ed83c7f9c1bd1d76890bfbb0ee34704a08d223b5
jingjie <vosonhung03@gmail.com>
2024-10-01 01:21:36 +0700

    refactor: separate components

--- src/pages/Authentication/SignUp.tsx
+++ src/pages/Authentication/SignUp.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
 import LogoDark from '../../images/logo/logo-dark.svg';
 import Logo from '../../images/logo/logo.svg';
 
-const SignUp: React.FC = () => {
+export default function SignUp() {
   return (
-    <>
-      <Breadcrumb pageName="Sign Up" />
-
+    <div className="flex justify-center items-center p-20">
       <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
         <div className="flex flex-wrap items-center">
           <div className="hidden w-full xl:block xl:w-1/2">
@@ -349,8 +345,7 @@ const SignUp: React.FC = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default SignUp;
